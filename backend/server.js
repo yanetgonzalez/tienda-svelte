9d541051b14786d93082dcbda05c6ebbb16a0f1b
yanet gonzalez <l231080477@iztapalapa.tecnm.mx>
2026-05-11 21:54:17 -0600

    arreglado backend

--- backend/server.js
+++ backend/server.js
@@ -116,6 +116,8 @@ app.get('/', (req, res) => {
 // =======================
 // SERVIDOR
 // =======================
-app.listen(3000, () => {
-  console.log("Servidor en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor funcionando 🚀");
 });
